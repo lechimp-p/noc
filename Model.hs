a54ded7c68d8f6fb90708a93e95515140127f34a
Richard Klees <richard.klees@rwth-aachen.de>
2014-07-29 19:39:03 +0200

    model main file

---
+++ Model.hs
@@ -0,0 +1,15 @@
+module Model
+    ( module Model.Operations
+    , mkNoC
+    , Operation
+    , mkLogin
+    , mkPassword
+    , mkName
+    , mkDesc
+    )
+where
+
+import Model.Operations
+import Model.NoC
+import Model.OpMonad
+import Model.BaseTypes
