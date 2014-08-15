e526b16aea1362ed0643347327b0dd36726e114a
Richard Klees <richard.klees@rwth-aachen.de>
2014-08-15 13:21:14 +0200

    defined MonadJSON

--- Server/API/JSONUtils.hs
+++ Server/API/JSONUtils.hs
@@ -5,8 +5,10 @@ module API.JSONUtils
     , maybeProp
     , prop
     , (<:)
-    , (<::) 
-    , JSONError'
+    , (<:.) 
+    , (.:>)
+    , (?:>)
+    , JSONError
     , runJSONMonadT
     , runJSONMonadT'
     )
@@ -23,15 +25,60 @@ import Control.Monad.IO.Class
 import Control.Lens
 import Control.Lens.Prism
 
-data JSONError'
+data JSONError
     = MissingProp Text
     | CantDecodeObject
     deriving (Show)
 
+class Monad m => MonadJSONError m where
+    throwJSONError :: JSONError -> m a
+
+class MonadJSONError m => MonadJSON m where
+    readProp  :: FromJSON a => Text -> m (Maybe a)
+    writeProp :: ToJSON a => Text -> a -> m () 
+
+infixl 9 <:
+infixl 9 <:.
+infixl 9 .:>
+infixl 9 ?:> 
+
+(<:) :: (ToJSON a, MonadJSON m) => Text -> a -> m a 
+s <: a = writeProp s a >> return a
+
+(<:.) :: (ToJSON a, MonadJSON m) => Text -> m a -> m a 
+n <:. m = do
+    val <- m
+    n <: val
+
+(.:>) :: (FromJSON a, MonadJSON m) => Text -> (a -> m b) -> m b
+n .:> m = do
+    p <- prop n
+    m p
+
+(?:>) :: (FromJSON a, MonadJSON m) => Text -> (a -> m b) -> m (Maybe b)
+n ?:> m = do
+    p' <- maybeProp n
+    case p' of
+        Nothing -> return Nothing
+        Just p  -> do
+            res <- m p
+            return (Just res) 
+
+maybeProp :: (FromJSON a, MonadJSON m) => Text -> m (Maybe a)
+maybeProp = readProp 
+
+prop :: (FromJSON a, MonadJSON m) => Text -> m a
+prop n = do 
+    p <- maybeProp n
+    case p of
+        Nothing -> throwJSONError $ MissingProp n
+        Just p' -> return p'
+
+
+
 data JSONMonadT m a where
-    Get :: (FromJSON a, Monad m) => Text -> JSONMonadT m (Maybe a)
-    Set :: (ToJSON a, Monad m) => Text -> a -> JSONMonadT m ()
-    Throw :: Monad m => JSONError' -> JSONMonadT m a 
+    ReadProp :: (FromJSON a, Monad m) => Text -> JSONMonadT m (Maybe a)
+    WriteProp :: (ToJSON a, Monad m) => Text -> a -> JSONMonadT m ()
     Return :: Monad m => a -> JSONMonadT m a
     Bind :: Monad m => JSONMonadT m a -> (a -> JSONMonadT m b) -> JSONMonadT m b
     Lift :: Monad m => m a -> JSONMonadT m a
@@ -43,48 +90,31 @@ instance Monad m => Monad (JSONMonadT m) where
 instance MonadTrans JSONMonadT where
     lift = Lift
 
-runJSONMonadT :: (Monad m) 
-              => JSONMonadT m a -> ByteString -> m (Either JSONError' (a, Value))
+instance MonadJSONError m => MonadJSONError (JSONMonadT m) where
+    throwJSONError = lift . throwJSONError
+
+instance MonadJSONError m => MonadJSON (JSONMonadT m) where
+    readProp = ReadProp
+    writeProp = WriteProp
+
+runJSONMonadT :: (MonadJSONError m) 
+              => JSONMonadT m a -> ByteString -> m (a, Value)
 runJSONMonadT m bs = 
     case decode bs of
-        Nothing -> return . Left $ CantDecodeObject
-        Just obj -> runJSONMonadT' m obj [] >>= return . over (_Right . _2) object
+        Nothing -> throwJSONError CantDecodeObject
+        Just obj -> runJSONMonadT' m obj [] >>= return . over _2 object
 
-runJSONMonadT' :: (Monad m)
-               => JSONMonadT m a -> Object -> [Pair] -> m (Either JSONError' (a, [Pair]))
+runJSONMonadT' :: (MonadJSONError m)
+               => JSONMonadT m a -> Object -> [Pair] -> m (a, [Pair])
 runJSONMonadT' m obj ps =
     case m of
-        Set prop val    -> return $ Right ((), (prop, toJSON val) : ps)
-        Throw err       -> return $ Left err
-        Return val      -> return $ Right (val, ps)
-        Lift m          -> m >>= \ v -> return $ Right (v, ps)
-        Get prop        -> 
+        WriteProp p v   -> return ((), (p, toJSON v) : ps)
+        Return val      -> return (val, ps)
+        Lift m          -> m >>= \ v -> return (v, ps)
+        ReadProp prop   -> 
             let value = HM.lookup prop obj  -- Maybe Value
                 result = fmap (parseMaybe parseJSON) value -- Maybe Maybe a
-            in return $ Right (join result, ps)
+            in return $ (join result, ps)
         Bind v f        -> do
-            v' <- runJSONMonadT' v obj ps 
-            case v' of
-                Left err -> return $ Left err
-                Right (res, ps') -> runJSONMonadT' (f res) obj (ps' ++ ps)
-
-maybeProp :: (FromJSON a, Monad m) => Text -> JSONMonadT m (Maybe a)
-maybeProp = Get
-
-prop :: (FromJSON a, Monad m) => Text -> JSONMonadT m a
-prop n = do 
-    p <- maybeProp n
-    case p of
-        Nothing -> Throw $ MissingProp n
-        Just p' -> return p'
-
-infixl 9 <:
-infixl 9 <::
-
-(<:) :: (ToJSON a, Monad m) => Text -> a -> JSONMonadT m ()
-(<:) = Set
-
-(<::) :: (ToJSON a, Monad m) => Text -> JSONMonadT m a -> JSONMonadT m ()
-n <:: m = do
-    val <- m
-    Set n val
+            (v, ps') <- runJSONMonadT' v obj ps 
+            runJSONMonadT' (f v) obj (ps' ++ ps)
