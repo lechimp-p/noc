05dc1c4c719c4ee9afc333a30ccd2297a0852d00
Richard Klees <richard.klees@rwth-aachen.de>
2014-08-26 21:33:59 +0200

    parametrized creation tests.

--- python/steps/steps.py
+++ python/steps/steps.py
@@ -5,8 +5,8 @@ def step_impl(context, user):
     if not user in context.users:
         try:
             context.users[user] = context.NoC.user(user, user)
-        except NoCError:
-            context.users[user] = context.NoC.user.create(self.context.users["admin"], user, user)
+        except context.NoC.NoCError:
+            context.users[user] = context.NoC.user.create(context.users["admin"], user, user)
 
 @when("I am {user}")
 def step_impl(context, user):
@@ -19,11 +19,31 @@ def step_impl(context, what):
     else:
         raise ValueError("Don't know how i could possibly be %s." % what)
 
-@then("I can create a {what} \"{name}\"")
-def step_impl(context, what, name):
+@then("I {can} create a {what} \"{name}\"")
+def step_impl(context, can, what, name):
+    if can == "can":
+        mod = id
+    elif can == "can not":
+        mod = lambda x : not x
+    else:
+        raise ValueError("Either you can or you can not, but i don't know how you '%s'" % can)
+        
     if what == "channel":
-        context.channels[name] = context.NoC.channel.create(context.actor, name, "Description of %s" % name)
+        try:
+            context.channels[name] = context.NoC.channel.create(context.actor, name, "Description of %s" % name)
+            raised = False
+        except context.NoC.NoCError:
+            raised = True
+        return mod(raised)
+            
+        
     elif what == "user":
-        context.users[name] = context.NoC.user.create(context.actor, name, name)
+        try: 
+            context.users[name] = context.NoC.user.create(context.actor, name, name)
+            raised = False
+        except context.NoC.NoCError:
+            raised = True
+        return mod(raised)
+
     else:
         raise ValueError("Don't know how i could possibly create a %s" % what)
