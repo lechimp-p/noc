49fb8847d91dc161773dd86327671d2416b37852
Richard Klees <richard.klees@rwth-aachen.de>
2014-08-30 12:41:52 +0200

    introduced 1 s sleep after NoC-Server-dev startup in tests to make the server available when the tests start.

--- python/environment.py
+++ python/environment.py
@@ -3,14 +3,17 @@ import subprocess
 import shutil 
 import os.path as path
 import os
+import time
 
 def before_all(context):
     context.server_process = subprocess.Popen([path.abspath("../Server/dist/build/NoC-Server-dev/NoC-Server-dev")])
+    time.sleep(1)
 
 def after_all(context):
     context.server_process.terminate()
     context.server_process.wait()
     shutil.rmtree(path.abspath("./state"))   
+    shutil.rmtree(path.abspath("./files"))   
     os.remove(path.abspath("./client_session_key.aes"))
 
 def before_scenario(context, feature):
