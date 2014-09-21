c1cdb01cec26b601acbac81f08cefa46aa7082fd
Richard Klees <richard.klees@rwth-aachen.de>
2014-09-21 13:34:47 +0200

    temporarily added old Operations.hs to reuse code for simple model.

---
+++ Server/Model/Simple/_Operations.hs
@@ -0,0 +1,289 @@
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+module Model.Operations
+    ( Model.Operations.getOperatorId
+    , Model.Operations.addAdmin
+    , Model.Operations.rmAdmin
+    , getChanName
+    , getChanDesc
+    , getChanType
+    , setChanName
+    , setChanDesc
+    , setChanType
+    , addChanOwner
+    , addChanProducer
+    , addChanConsumer
+    , rmChanOwner
+    , rmChanProducer
+    , rmChanConsumer
+    , amountOfDistinctUsers
+    , lastPostTimestamp
+    , subscribeToChan
+    , unsubscribeFromChan
+    , getUserByLogin
+    , getUserLogin
+    , getUserName
+    , getUserDesc
+    , getUserIcon
+    , getUserOwnedChannels
+    , getUserSubscriptions
+    , getUserContacts
+    , addUserContact
+    , rmUserContact
+    , getUserNotifications
+    , addUserNotification
+    , tryToAddUserNotification
+    , setUserLogin
+    , setUserPassword
+    , setUserName
+    , setUserDesc
+    , setUserIcon
+    , createUser
+    , createChannel
+    , post
+    , Model.Operations.messages
+    , messagesTill
+    , Offset
+    , Amount
+    )
+where
+
+import qualified Data.Set as S
+import qualified Data.IxSet as IX
+import Data.Text hiding (drop, take, takeWhile)
+import Control.Lens
+import Control.Lens.Prism
+import Control.Applicative ((<$>))
+import Data.Time.Clock (UTCTime)
+import Data.Maybe (isJust)
+
+import Model.Errors
+import Model.Permissions
+import Model.BaseTypes
+import Model.OpMonad
+import Model.NoC
+import qualified Model.NoC as N
+import qualified Model.Channel as C
+import Model.Channel
+import qualified Model.User as U
+import Model.User 
+import qualified Model.Message as M
+import Model.Message 
+
+type SimpleLens a b = Lens a a b b 
+
+---------
+-- on noc
+---------
+
+getOperatorId :: OpMonad m => m UserId
+getOperatorId = ifIsLoggedIn' return 
+
+addAdmin :: OpMonad m => UserId -> m ()
+addAdmin uid = checkAccess () forNoCAdmins $ do
+    getUser uid
+    Model.OpMonad.addAdmin uid
+
+rmAdmin :: OpMonad m => UserId -> m ()
+rmAdmin uid = checkAccess () forNoCAdmins $ do
+    n <- fmap S.size getAdmins
+    OnlyOneNoCAdminLeft `throwOn` (n == 1) 
+    Model.OpMonad.rmAdmin uid
+
+-------------------------
+-- operations on channels
+-------------------------
+
+getFromChan :: OpMonad m => SimpleLens Channel a -> ChanId -> m a
+getFromChan l cid = checkAccess cid forAllChanPeople $ do
+    chan <- getChannel cid
+    return $ chan ^. l
+
+setToChan :: OpMonad m => SimpleLens Channel a -> ChanId -> a -> m ()
+setToChan l cid v = checkAccess cid forChanOwnersOrAdmins $ do
+    chan <- getChannel cid
+    storeChannel (set l v chan)  
+
+getChanName cid = getFromChan C.name cid
+getChanDesc cid = getFromChan C.desc cid
+getChanType cid = getFromChan C.type' cid
+
+setChanName cid v = setToChan C.name cid v
+setChanDesc cid v = setToChan C.desc cid v
+setChanType cid v = setToChan C.type' cid v
+
+addChanXX :: OpMonad m 
+          => SimpleLens Channel (S.Set UserId)
+          -> ChanId -> UserId -> m ()
+addChanXX l cid uid = checkAccess cid forChanOwnersOrAdmins $ do
+    chan <- getChannel cid
+    storeChannel (over l (S.insert uid) chan)
+
+rmChanXX :: OpMonad m 
+         => SimpleLens Channel (S.Set UserId)
+         -> ChanId -> UserId -> m ()
+rmChanXX l cid uid = checkAccess cid forChanOwnersOrAdmins $ do
+    chan <- getChannel cid
+    storeChannel (over l (S.delete uid) chan)
+
+addChanOwner cid uid = addChanXX C.owners cid uid
+addChanProducer cid uid = addChanXX C.producers cid uid
+addChanConsumer cid uid = addChanXX C.consumers cid uid
+
+rmChanOwner cid uid = checkAccess cid forChanOwnersOrAdmins $ do
+    chan <- getChannel cid
+    OnlyOneChannelOwnerLeft cid `throwOn` (S.size (chan ^. owners) == 0)
+    storeChannel (over C.owners (S.delete uid) chan)
+rmChanProducer cid uid = rmChanXX C.producers cid uid
+rmChanConsumer cid uid = rmChanXX C.consumers cid uid
+
+amountOfDistinctUsers :: OpMonad m => ChanId -> m Int
+amountOfDistinctUsers cid = checkAccess cid forAllChanPeople $ do
+    chan <- getChannel cid
+    return . S.size . S.unions $ fmap (chan &) [_owners, _producers, _consumers]    
+
+lastPostTimestamp :: OpMonad m => ChanId -> m (Maybe UTCTime)
+lastPostTimestamp cid = do
+    chan <- getChannel cid
+    let mids = C._messages chan
+    msg <- head' . IX.toDescList (IX.Proxy :: IX.Proxy UTCTime) <$> getMessages @= cid
+    return $ fmap _timestamp msg
+
+subscribeToChan :: OpMonad m => UserId -> ChanId -> m () 
+subscribeToChan uid cid = checkAccess cid forAllChanPeople $
+    checkAccess uid forUserSelfOrAdmins $ do
+        user <- getUser uid
+        storeUser (over U.subscriptions (S.insert cid) user)  
+      
+unsubscribeFromChan :: OpMonad m => UserId -> ChanId -> m ()
+unsubscribeFromChan uid cid = checkAccess uid forUserSelfOrAdmins $ do
+        user <- getUser uid
+        storeUser (over U.subscriptions (S.delete cid) user)
+
+----------------------
+-- operations on users
+----------------------
+
+getFromUser :: OpMonad m => SimpleLens User a -> UserId -> m a
+getFromUser l uid = ifIsLoggedIn $ do
+    user <- getUser uid
+    return $ user ^. l 
+
+setToUser :: OpMonad m => SimpleLens User a -> UserId -> a -> m ()
+setToUser l uid v = checkAccess uid forUserSelfOrAdmins $ do
+    user <- getUser uid
+    storeUser (set l v user) 
+
+overInUser :: OpMonad m => SimpleLens User a -> UserId -> (a -> a) -> m ()
+overInUser l uid fun = checkAccess uid forUserSelfOrAdmins $ do
+    user <- getUser uid
+    storeUser (over l fun user)
+
+getUserLogin uid = getFromUser U.login uid
+getUserName uid = getFromUser U.name uid
+getUserDesc uid = getFromUser U.desc uid
+getUserIcon uid = getFromUser U.icon uid
+getUserOwnedChannels uid = checkAccess uid forUserSelfOrAdmins
+    $ getFromUser U.ownedChannels uid
+getUserSubscriptions uid = checkAccess uid forUserSelfOrAdmins
+    $ getFromUser U.subscriptions uid
+getUserContacts uid = checkAccess uid forUserSelfOrAdmins
+    $ getFromUser U.contacts uid
+getUserNotifications uid offs amo = checkAccess uid forUserSelfOrAdmins
+    $ do
+        res <- getFromUser U.notifications uid
+        return . take amo . drop offs $ res
+
+addUserNotification uid notf = checkAccess uid forUserContactsSelfOrAdmins $ do
+    user <- getUser uid
+    storeUser (over U.notifications (notf :) user)
+
+tryToAddUserNotification uid notf = tryAccess uid forUserContactsSelfOrAdmins $ do
+    user <- getUser uid
+    storeUser (over U.notifications (notf :) user)
+
+addUserContact uid other = overInUser U.contacts uid (S.insert other)  
+rmUserContact uid other = overInUser U.contacts uid (S.delete other)  
+
+setUserLogin uid l = do
+    oid <- Model.Operations.getOperatorId
+    l' <- getUserLogin oid
+    if uid == oid && l == l'
+        then return ()
+        else do
+            checkDuplicateLogin l
+            setToUser U.login uid l
+setUserName uid v = setToUser U.name uid v
+setUserPassword uid v = setToUser U.password uid v
+setUserDesc uid v = setToUser U.desc uid v
+setUserIcon uid v = setToUser U.icon uid v
+
+getUserByLogin :: OpMonad m => Text -> m UserId
+getUserByLogin l = ifIsLoggedIn $ do
+    user <- IX.getOne <$> getUsers @= (Login l)
+    case user of
+        (Just user) -> return $ U._id user
+        otherwise -> throw $ UnknownLogin l 
+
+--------------------
+-- creation of users
+--------------------
+
+createUser :: OpMonad m => Login -> Password -> m UserId
+createUser l pw = checkAccess () forNoCAdmins $ do
+    uid <- newUserId
+    storeUser $ User uid l pw (mkName "") (mkDesc "") Nothing S.empty S.empty S.empty []
+    return uid
+
+-----------------------
+-- creation of channels
+-----------------------
+
+createChannel :: OpMonad m => Name -> Desc -> m ChanId
+createChannel n d = ifIsLoggedIn $ do
+    cid <- newChanId
+    oid <- Model.Operations.getOperatorId
+    user <- getUser oid
+    storeChannel $ Channel cid n d None (S.insert oid S.empty) S.empty S.empty S.empty
+    storeUser $ over U.ownedChannels (S.insert cid) user
+    return cid
+
+----------------------
+-- posting of messages
+----------------------
+
+post :: OpMonad m => ChanId -> UTCTime -> Text -> Maybe Image -> m MsgId
+post cid ts txt img = checkAccess cid forProducersOrOwners $ do
+    mid <- newMsgId 
+    oid <- Model.Operations.getOperatorId
+    chan <- getChannel cid
+    storeChannel $ over C.messages (S.insert mid) chan
+    storeMessage $ Message mid cid img txt oid ts
+    return mid
+
+----------------------
+-- reading of messages
+----------------------
+
+type Offset = Int
+type Amount = Int
+
+messages :: OpMonad m => ChanId -> Offset -> Amount -> m [Message]
+messages cid ofs am = checkAccess cid forConsumersOrOwners $ do
+    take am . drop ofs . IX.toDescList (IX.Proxy :: IX.Proxy UTCTime) <$> getMessages @= cid  
+
+messagesTill :: OpMonad m => ChanId -> UTCTime -> m [Message]
+messagesTill cid ts = checkAccess cid forConsumersOrOwners $ do
+    takeWhile ((<=) ts . _timestamp) . IX.toDescList (IX.Proxy :: IX.Proxy UTCTime) <$> getMessages @= cid
+
+----------
+-- helpers
+----------
+
+checkDuplicateLogin l = do
+    n <- IX.size <$> getUsers @= l 
+    DuplicateLogin l `throwOn` (n >= 1) 
+
+head' []     = Nothing
+head' (x:_)  = Just x
