49add6eecfcae81c072c7cd7c190dcb8b1faf055
Richard Klees <richard.klees@rwth-aachen.de>
2014-09-27 18:29:41 +0200

    removed safecopy instances of basetypes.

--- Server/Model/BaseTypes.hs
+++ Server/Model/BaseTypes.hs
@@ -24,23 +24,22 @@ where
 import Data.Text
 import Data.Time.Clock
 import Data.Data (Data, Typeable) 
-import Data.SafeCopy (SafeCopy, base, deriveSafeCopy)
 
-newtype UserId = UserId { uiToInt :: Int } deriving (Show, Read, Eq, Ord, Data, Typeable, SafeCopy)
-newtype ChanId = ChanId { ciToInt :: Int } deriving (Show, Read, Eq, Ord, Data, Typeable, SafeCopy)
-newtype MsgId = MsgId { miToInt :: Int } deriving (Show, Read, Eq, Ord, Data, Typeable, SafeCopy)
+newtype UserId = UserId { uiToInt :: Int } deriving (Show, Read, Eq, Ord, Data, Typeable)
+newtype ChanId = ChanId { ciToInt :: Int } deriving (Show, Read, Eq, Ord, Data, Typeable)
+newtype MsgId = MsgId { miToInt :: Int } deriving (Show, Read, Eq, Ord, Data, Typeable)
 
-newtype Login = Login { loginToText :: Text } deriving (Show, Read, Eq, Ord, Data, Typeable, SafeCopy)
+newtype Login = Login { loginToText :: Text } deriving (Show, Read, Eq, Ord, Data, Typeable)
 mkLogin = Login
-newtype Password = Password { pwToText :: Text } deriving (Show, Read, Eq, Ord, Data, Typeable, SafeCopy)
+newtype Password = Password { pwToText :: Text } deriving (Show, Read, Eq, Ord, Data, Typeable)
 mkPassword = Password
-newtype Name = Name { nameToText :: Text } deriving (Data, Typeable, SafeCopy)
+newtype Name = Name { nameToText :: Text } deriving (Data, Typeable)
 mkName = Name
-newtype Desc = Desc { descToText :: Text } deriving (Data, Typeable, SafeCopy)
+newtype Desc = Desc { descToText :: Text } deriving (Data, Typeable)
 mkDesc = Desc
 
-newtype Image = Image { imgPath :: Text } deriving (Data, Typeable, SafeCopy)
-newtype Icon = Icon { icnPath :: Text } deriving (Data, Typeable, SafeCopy)
+newtype Image = Image { imgPath :: Text } deriving (Data, Typeable)
+newtype Icon = Icon { icnPath :: Text } deriving (Data, Typeable)
 
 checkPassword :: Password -> Password -> Bool
 checkPassword pw pw' = pwToText pw == pwToText pw'
