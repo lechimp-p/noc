ec8d84a96bb6f4cef0eab4414e170696ffcac261
Richard Klees <richard.klees@rwth-aachen.de>
2014-09-28 14:48:37 +0200

    implementation of acid state in terms of extensible effects seems to work out...

--- Server/Model/Acid.hs
+++ Server/Model/Acid.hs
@@ -1,4 +1,7 @@
-module ACID 
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE FlexibleContexts #-}
+
+module Model.Acid
     ( runQuery
     , runQueryAndUpdate
     , runSimple
@@ -14,43 +17,88 @@ import Model.Exec
 import Model.Simple.NoC (NoC)
 import qualified Model.Acid.Query as AQ
 
-{--import qualified Model.Simple.NoC as N
-import qualified Model.Simple.User as U
-import Model.Simple.User
-import qualified Model.Simple.Channel as C
-import Model.Simple.Channel
-import qualified Model.Simple.Message as M
-import Model.Simple.Message
---}
 import Control.Lens 
 import Control.Eff
 import Control.Eff.Lift
+import Control.Monad.IO.Class
 import qualified Data.Acid as A
 import qualified Data.Acid.Advanced as AA
 import qualified Data.Set as S 
 import qualified Data.IxSet as IX
+import Data.Typeable.Internal (Typeable1)
 import Data.Time.Clock (UTCTime)
 
-data Effect
-    = Go (VE r w)
-    | Lift (m (Either Error a)) (a -> Either Error (VE r w))
-    | Throw Error
 
-runQuery :: (MonadIO m, SetMember Lift (Lift m) r)
-         => A.AcidState NoC -> Eff (Query :> Exec :> r) -> Eff (Exec :> r) a
-runQuery state action = go state (admin action)
+runQuery :: (Typeable1 m, Functor m, MonadIO m, SetMember Lift (Lift m) r)
+         => A.AcidState NoC -> Eff (Query :> Exec :> r) a -> Eff (Exec :> r) a
+runQuery state action = go (admin action)
     where
-    go _ (Val c) = return v
-    go noc (E request) = handleRelay request (go noc)
-        $ \ req -> case evalQuery state req of
-            Go next -> go state next
-            Lift act next -> do
+    go (Val v) = return v
+    go (E request) = handleRelay request go
+        $ \ req -> let act = evalQuery state req in do
                 res <- lift act 
-                either throwME (either throwME (go state)) res
-            Throw err -> throwME err
+                either throwME go res
 
-evalQuery :: (Member Query r)
-          => A.AcidState NoC -> Query (VE r w) -> 
+evalQuery :: (Functor m, MonadIO m, Member Query r)
+          => A.AcidState NoC -> Query (VE r w) -> m (Either Error (VE r w))
 evalQuery state q = case q of
-    IsAdmin uid next
-        -> Lift (AA.query' state AQ.IsAdmin) next
+    IsAdmin uid next -> ffq next (AQ.IsAdmin uid)
+    CountAdmins next -> ffq next AQ.CountAdmins
+    GetUserIdByLogin l next -> ffq next (AQ.GetUserIdByLogin l)
+    {--ChanQuery cid q 
+        -> evalChanQuery noc q cid
+    UserQuery uid q 
+        -> evalUserQuery noc q uid
+    --}
+    where
+    ffq n = fmap (fmap n) . AA.query' state
+
+
+runQueryAndUpdate :: (Typeable1 m, Functor m, MonadIO m, SetMember Lift (Lift m) r)
+                  => A.AcidState NoC -> Eff (Query :> Update :> Exec :> r) a -> Eff (Exec :> r) a
+runQueryAndUpdate state action = go (admin action)
+    where
+    go (Val v) = return v
+    go (E request) = checkQuery request
+
+    checkQuery r = flip (either checkUpdate) (decomp r)
+                        $ \ req -> let act = evalQuery state req in do
+                                res <- lift act 
+                                either throwME go res
+    checkUpdate r = flip (either passOn) (decomp r)
+                        $ \ req -> let act = evalUpdate state req in do
+                                res <- lift act 
+                                either throwME go res
+    passOn r = send (flip fmap r) >>= go 
+
+evalUpdate :: (Functor m, MonadIO m, Member Query r)
+          => A.AcidState NoC -> Update (VE r w) -> m (Either Error (VE r w))
+evalUpdate state q = undefined
+
+
+runSimple :: (Typeable1 m, Functor m, MonadIO m, SetMember Lift (Lift m) r)
+          => A.AcidState NoC -> Maybe UserId -> Eff (Query :> Update :> Exec :> r) a -> Eff r (Either Error a)
+runSimple state uid action = go uid (admin action)
+    where
+    go _ (Val v) = return . Right $ v
+    go uid (E request) = checkQuery uid request 
+ 
+    checkQuery u r = flip (either (checkUpdate uid)) (decomp r)
+                        $ \ req -> let act = evalQuery state req in do
+                                res <- lift act 
+                                either (return . Left) (go u) res
+    checkUpdate u r = flip (either (checkExec uid)) (decomp r)
+                        $ \ req -> let act = evalUpdate state req in do
+                                res <- lift act 
+                                either (return . Left) (go u) res
+    checkExec u r = flip (either (passOn u)) (decomp r)
+                        $ \ req -> let act = evalExec state u req in do
+                                res <- lift act 
+                                either (return . Left) (uncurry go) res
+    passOn u r = send (flip fmap r) >>= go u 
+
+evalExec :: (Functor m, MonadIO m, Member Query r)
+          => A.AcidState NoC -> Maybe UserId 
+          -> Exec (VE r w) -> m (Either Error (Maybe UserId , (VE r w)))
+evalExec state uid q = undefined
+
--- Server/Model/Acid/Query.hs
+++ Server/Model/Acid/Query.hs
@@ -1,8 +1,108 @@
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeFamilies #-}
 
-module ACID.Query
+module Model.Acid.Query
 where
 
+import Model.BaseTypes
+import Model.Message
+import Model.Errors
+import Model.Simple.Operations
+import Model.Acid.Safecopy
+
+import Data.Acid
+import Data.Text (Text)
+import qualified Data.Set as S
+import Control.Monad.Reader (ask)
+import Data.Time.Clock (UTCTime)
+
+isAdmin :: UserId -> Query NoC (Either Error Bool)
+isAdmin uid = ask >>= \ noc -> return . Right $ isAdminR noc uid
+
+countAdmins :: Query NoC (Either Error Int)
+countAdmins = ask >>= \ noc -> return . Right $ countAdminsR noc
+
+getUserIdByLogin :: Text -> Query NoC (Either Error (Maybe UserId))
+getUserIdByLogin t = ask >>= \ noc -> return . Right $ getUserIdByLoginR noc t
+
+getChanName :: ChanId -> Query NoC (Either Error Name)
+getChanName cid = ask >>= \ noc -> return $ getChanNameR noc cid
+
+getChanDesc :: ChanId -> Query NoC (Either Error Desc)
+getChanDesc cid = ask >>= \ noc -> return $ getChanDescR noc cid
+
+getChanType :: ChanId -> Query NoC (Either Error ChanType)
+getChanType cid = ask >>= \ noc -> return $ getChanTypeR noc cid
+
+getChanImage :: ChanId -> Query NoC (Either Error (Maybe Image))
+getChanImage cid = ask >>= \ noc -> return $ getChanImageR noc cid
+
+isChanOwner :: ChanId -> UserId -> Query NoC (Either Error Bool)
+isChanOwner cid uid = ask >>= \ noc -> return $ isChanOwnerR noc cid uid
+
+isChanProducer :: ChanId -> UserId -> Query NoC (Either Error Bool)
+isChanProducer cid uid = ask >>= \ noc -> return $ isChanProducerR noc cid uid
+
+isChanConsumer :: ChanId -> UserId -> Query NoC (Either Error Bool)
+isChanConsumer cid uid = ask >>= \ noc -> return $ isChanConsumerR noc cid uid
+
+amountOfSubscribedUsers :: ChanId -> Query NoC (Either Error Int)
+amountOfSubscribedUsers cid = ask >>= \ noc -> return $ amountOfSubscribedUsersR noc cid
+
+lastPostTimestamp :: ChanId -> Query NoC (Either Error (Maybe UTCTime))
+lastPostTimestamp cid = ask >>= \ noc -> return $ lastPostTimestampR noc cid
+
+messages :: ChanId -> Offset -> Amount -> Query NoC (Either Error [Message])
+messages cid ofs am = ask >>= \ noc -> return $ messagesR noc cid ofs am
+
+messagesTill :: ChanId -> UTCTime -> Query NoC (Either Error [Message])
+messagesTill cid ts = ask >>= \ noc -> return $ messagesTillR noc cid ts
+
+getUserLogin :: UserId -> Query NoC (Either Error Login)
+getUserLogin uid = ask >>= \ noc -> return $ getUserLoginR noc uid
+
+getUserName :: UserId -> Query NoC (Either Error Name)
+getUserName uid = ask >>= \ noc -> return $ getUserNameR noc uid
+
+getUserDesc :: UserId -> Query NoC (Either Error Desc)
+getUserDesc uid = ask >>= \ noc -> return $ getUserDescR noc uid
+
+getUserIcon :: UserId -> Query NoC (Either Error (Maybe Icon))
+getUserIcon uid = ask >>= \ noc -> return $ getUserIconR noc uid
+
+getUserNotifications :: UserId -> Query NoC (Either Error [Notification])
+getUserNotifications uid = ask >>= \ noc -> return $ getUserNotificationsR noc uid
+
+getUserContacts :: UserId -> Query NoC (Either Error (S.Set UserId))
+getUserContacts uid = ask >>= \ noc -> return $ getUserContactsR noc uid
+
+getUserSubscriptions :: UserId -> Query NoC (Either Error (S.Set ChanId))
+getUserSubscriptions uid = ask >>= \ noc -> return $ getUserSubscriptionsR noc uid
+
+$(makeAcidic ''NoC  [ 'isAdmin
+                    , 'countAdmins
+                    , 'getUserIdByLogin
+                    , 'getChanName
+                    , 'getChanDesc
+                    , 'getChanType
+                    , 'getChanImage
+                    , 'isChanOwner
+                    , 'isChanProducer
+                    , 'isChanConsumer
+                    , 'amountOfSubscribedUsers
+                    , 'lastPostTimestamp
+                    , 'messages
+                    , 'messagesTill
+                    , 'getUserLogin
+                    , 'getUserName
+                    , 'getUserIcon
+                    , 'getUserNotifications
+                    , 'getUserContacts
+                    , 'getUserSubscriptions
+                    ])
+
+{--
 import Control.Monad.State.Strict
 import Control.Monad.Reader (ask)
 import Control.Monad.Trans.Class
@@ -18,7 +118,6 @@ import Data.Time.Clock (UTCTime)
 
 import Model 
 import qualified Model.Operations as O
-import Model.OpMonad 
 import Model.Simple (runOp', _operator)
 import Model.Errors
 
@@ -108,3 +207,4 @@ messagesQ oid c o = getQuery oid . O.messages c o
 
 messagesTillQ :: Maybe UserId -> ChanId -> UTCTime -> Query NoC (Either Error [Message], Maybe UserId)
 messagesTillQ oid c = getQuery oid . O.messagesTill c
+--}
