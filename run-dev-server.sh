cf67a28c041bdc3d169f52a5a81a5a3c1df36702
Richard Klees <richard.klees@rwth-aachen.de>
2014-10-09 13:58:10 +0200

    run-dev-server works =)

--- run-dev-server.sh
+++ run-dev-server.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-docker run -t -i -d -p 8000:8000 --name noc-server -v /opt/NoC-data:./NoC-data "cd /opt/NoC-data && /opt/NoC-Server/dist/build/NoC-Server-dev/NoC-Server-dev"
+docker run -t -i -p 8000:8000 --name noc-server -v $(pwd)/NoC-data:/opt/NoC-data noc-server /bin/sh -c 'cd /opt/NoC-data && /opt/NoC-Server/dist/build/NoC-Server-dev/NoC-Server-dev'
+docker stop noc-server
+docker rm noc-server
