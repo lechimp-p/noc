b6465cf517849b856009b66bdb0b54dc2ebd0e55
Richard Klees <richard.klees@rwth-aachen.de>
2014-10-09 12:35:44 +0200

    added screen logger for dev-server.

--- Server/DevMain.hs
+++ Server/DevMain.hs
@@ -8,11 +8,19 @@ import API.Config
 import Model
 import Model.Acid
 
+import Data.Time.Format
+import System.Locale
 import Control.Lens
 import Web.Routes.Happstack (implSite)
 import Happstack.Server (simpleHTTP, nullConf, Conf (..))
 
 initialNoC = mkNoC (mkLogin "admin") (mkPassword "admin") 
+
+accessLog :: FormatTime time
+          => String -> String -> time -> String -> Int -> Integer -> String -> String -> IO ()
+accessLog host user time request response size referer useragent =
+    putStrLn $ formatTime defaultTimeLocale "%T" time 
+               ++ " - " ++ request ++ " : " ++ show response
 
 main :: IO Int 
 main = do
@@ -21,6 +29,7 @@ main = do
         withACID (_acidPath cfg) initialNoC $ \acid -> do
             simpleHTTP (nullConf { Happstack.Server.port = cfg ^. serverConfig . API.Config.port
                                  , timeout = cfg ^. serverConfig . threadTimeout
+                                 , logAccess = Just accessLog 
                                  }
                        )
                        $ runAcidAPISite cfg acid
