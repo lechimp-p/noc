d04161500bfab6a670280af6cacf560f0d0b7102
Richard Klees <richard.klees@rwth-aachen.de>
2014-10-13 01:05:14 +0200

    docker: finetuning

--- docker/make-images.sh
+++ docker/make-images.sh
@@ -5,7 +5,7 @@ git clone ../.. src
 docker build -t noc-base .
 rm -r src* 
 cd ..
-docker run -t --name noc-build -v $(pwd)/..:/opt/NoC-Server-git noc-base /bin/sh /opt/install-noc-server.sh 
+docker run -t -i --name noc-build -v $(pwd)/..:/opt/NoC-Server-git noc-base /bin/sh /opt/install-noc-server.sh 
 docker commit noc-build noc-installed
 docker stop noc-build
 docker rm noc-build
--- docker/run-dev-server.sh
+++ docker/run-dev-server.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
 mkdir data
-docker run -t -i -d -p 8000:8000 --name noc-server \
-           -v $(pwd)/data:/opt/NoC-data \
-           noc-server
+docker run -d --name noc-server \
+           -v $(pwd)/data:/opt/NoC-data:rw \
+           --sig-proxy=true \
+           noc-installed \
+           /opt/NoC-Server-src/Server/dist/build/NoC-Server-dev/NoC-Server-dev -c /opt/NoC.conf
 docker run -d -p 80:80 --name nginx-server \
            -v $(pwd)/../HTML2/dist:/usr/share/nginx/html:ro \
-           -v $(pwd)/data:/opt/NoC-data \
-           --link noc-server:noc-server
+           -v $(pwd)/data:/opt/NoC-data:ro \
+           --link noc-server:noc-server \
            noc-nginx
-docker attach noc-server 
+docker logs --follow=true --timestamps=true noc-server 
+echo "  Shutting down..."
 docker stop noc-server
 docker rm noc-server
 docker stop nginx-server
