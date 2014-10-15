585e4cd1410bce638c6d867642c2cb19185779ad
Richard Klees <richard.klees@rwth-aachen.de>
2014-10-15 17:52:23 +0200

    fixed bug in rmUserSubscriptionsR

--- Server/Model/Simple/Operations.hs
+++ Server/Model/Simple/Operations.hs
@@ -157,7 +157,7 @@ addUserSubscriptionR n u cid = do
     (n', _) <- updateChan (over subscribers (S.insert u)) () n cid
     updateUser (over subscriptions (S.insert cid)) () n u
 rmUserSubscriptionR n u cid = do
-    (n',_) <- updateChan (over subscribers (S.insert u)) () n cid
+    (n',_) <- updateChan (over subscribers (S.delete u)) () n cid
     updateUser (over subscriptions (S.delete cid)) () n' u
 
     
