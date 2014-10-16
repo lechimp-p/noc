1f93003b7d93bd2545546368bea6a0cb33d10c4b
Richard Klees <richard.klees@rwth-aachen.de>
2014-10-16 09:55:11 +0200

    docker corrected update script

--- docker/update-images.sh
+++ docker/update-images.sh
@@ -5,6 +5,7 @@ docker start noc-data
 docker run -d -p 50022:22 --name noc-ssh \
            --volumes-from noc-data \
            noc-ssh
+sleep 1s
 git push -f devsrv master
 scp -P 50022 noc-data/NoC.conf root@localhost:/opt/NoC-data/NoC.conf
 docker stop noc-ssh
@@ -14,6 +15,7 @@ docker run -t -i --name noc-build \
            --volumes-from noc-data \
            noc-server \
            /bin/sh /opt/update-noc-server.sh 
+docker stop noc-build
 docker rm noc-build
 
 
