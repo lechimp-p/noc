44d79f32e461597005ecd0578a8b3f718b0a11c5
Richard Klees <richard.klees@rwth-aachen.de>
2014-11-01 20:18:34 +0100

    ng: profile starts to work.

--- HTML2/custom/views/profile/profile.js
+++ HTML2/custom/views/profile/profile.js
@@ -1,5 +1,19 @@
 angular.module("NoC.profile", []).
-controller("profile-controller", [ "$scope", "model"
-         , function($scope, model) {
+controller("profile-controller", [ "$scope", "$routeParams", "model"
+         , function($scope, $routeParams, model) {
     "use strict";
+
+    $scope.user = { id : $routeParams.userId };
+
+    $scope.addToContacts = function() {
+    };
+
+    $scope.removeFromContacts = function() {
+    };
+
+    model.user($scope.user.id).onUpdate(function(response) {
+        $scope.user = response;
+    });
+
+    model.user($scope.user.id).update();
 }]);
