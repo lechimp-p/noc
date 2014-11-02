93b13644851e69b9f2481a5e62d3e1dc1a3ba44d
Richard Klees <richard.klees@rwth-aachen.de>
2014-11-02 15:10:48 +0100

    my profile works

--- HTML2/custom/views/my-profile/my-profile.js
+++ HTML2/custom/views/my-profile/my-profile.js
@@ -2,4 +2,22 @@ angular.module("NoC.my-profile", []).
 controller("my-profile-controller", [ "$scope", "model", "user"
          , function($scope, model, user) {
     "use strict";
+
+    var init = function(id) {
+        $scope.user = { id : id }; 
+
+        model.user($scope.user.id).onUpdate(function(response) {
+            $scope.user = response;
+        });
+
+        model.user($scope.user.id).update();
+    };
+
+    var userId = user.getId();
+    if (typeof userId == "undefined") {
+        user.onIdAcquired(init);
+    }
+    else {
+        init(userId);
+    }
 }]);
