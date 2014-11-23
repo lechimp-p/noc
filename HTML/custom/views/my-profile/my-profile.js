ff08e56c29f4489618d319a1ee4a5de9b80f8fff
Richard Klees <richard.klees@rwth-aachen.de>
2014-11-23 14:53:53 +0100

    ng: barely working implementation of auto updating fields in profile.

--- HTML/custom/views/my-profile/my-profile.js
+++ HTML/custom/views/my-profile/my-profile.js
@@ -1,13 +1,38 @@
 angular.module("NoC.my-profile", []).
-controller("my-profile-controller", [ "$scope", "model", "user"
-         , function($scope, model, user) {
+controller("my-profile-controller", [ "$scope", "$timeout", "model", "user"
+         , function($scope, $timeout, model, user) {
     "use strict";
+
+    var throttleDelay = 800; // ms
+
+    var throttledUpdate = function(key) {
+        var updateState = { timeout : null };       
+        
+        $scope.$watch("user."+key, function(nval, oval) {
+            if (_.isEqual(nval, oval)) {
+                return;
+            }
+
+            if (updateState.timeout !== null) {
+                $timeout.cancel(updateState.timeout);
+                updateState.timeout = null;
+            }
+
+            updateState.timeout = $timeout(function() {
+                var upd = {};
+                upd[key] = nval;
+                model.user($scope.user.id).set(upd);         
+                updateState.timeout = null;
+            }, throttleDelay);
+        });
+    };
 
     user.onIdAcquired(function(id) {
         $scope.user = { id : id }; 
 
-        model.user($scope.user.id).onUpdate(function(response) {
+        model.user($scope.user.id).get().success(function(response) {
             $scope.user = response;
+            //$scope.user.password = "xxxxxxxx";
             // TODO: This works around bad formed output of server.
             //       Should be fixed there...
             if ($scope.user.email.length === 0) {
@@ -16,8 +41,10 @@ controller("my-profile-controller", [ "$scope", "model", "user"
             else {
                 $scope.user.email = $scope.user.email[0];
             }
-        });
 
-        model.user($scope.user.id).update();
+            throttledUpdate("name");
+            throttledUpdate("description");
+            throttledUpdate("email");
+        });
     });
 }]);
