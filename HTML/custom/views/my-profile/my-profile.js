7b1038e5c3cd038fe8b094a9afe3a8ebd2f3cf54
Richard Klees <richard.klees@rwth-aachen.de>
2014-11-25 20:09:56 +0100

    ng: password field on my-profile works.

--- HTML/custom/views/my-profile/my-profile.js
+++ HTML/custom/views/my-profile/my-profile.js
@@ -7,6 +7,22 @@ controller("my-profile-controller", [ "$scope", "$timeout", "model", "user"
 
     $scope.updating = {};
     $scope.error = {};
+    $scope.confirmation = {};
+    $scope.beforeChange = {};
+
+    var update = function(key, val) {
+        var upd = {};
+        upd[key] = val;
+        model.user($scope.user.id).set(upd)
+             .success(function(_) {
+                $scope.updating[key] = false;
+             })         
+             .errorHandler(function(data, status, headers, config) {
+                $scope.error[key] = true;
+                $scope.updating[key] = false;
+                return true;
+             });
+    };
 
     var throttledUpdate = function(key) {
         var updateState = { timeout : null };       
@@ -27,20 +43,36 @@ controller("my-profile-controller", [ "$scope", "$timeout", "model", "user"
             }
 
             updateState.timeout = $timeout(function() {
-                var upd = {};
-                upd[key] = nval;
-                model.user($scope.user.id).set(upd)
-                    .success(function(_) {
-                        $scope.updating[key] = false;
-                    })         
-                    .errorHandler(function(data, status, headers, config) {
-                        $scope.error[key] = true;
-                        $scope.updating[key] = false;
-                        return true;
-                    });
-                updateState.timeout = null;
+               update(key, nval);
+               updateState.timeout = null;
             }, throttleDelay);
         });
+    };
+
+    var confirmedUpdate = function(key) {
+        // No need for confirmation on startup.
+        $scope.confirmation[key] = false;        
+
+        $scope.$watch("user."+key, function(nval, oval) {
+            if (_.isEqual(nval, oval) || $scope.confirmation[key]) {
+                return;
+            }
+             
+            // Now we need a confirmation...
+            $scope.confirmation[key] = true;
+            $scope.beforeChange[key] = oval;
+        });
+    };
+
+    $scope.confirm = function(key) {
+        $scope.confirmation[key] = false;
+        $scope.updating[key] = true;
+        update(key, $scope.user[key]);    
+    };
+
+    $scope.abort = function(key) {
+        $scope.confirmation[key] = false;
+        $scope.user[key] = $scope.beforeConfirmation;
     };
 
     user.onIdAcquired(function(id) {
@@ -48,11 +80,15 @@ controller("my-profile-controller", [ "$scope", "$timeout", "model", "user"
 
         model.user($scope.user.id).get().success(function(response) {
             $scope.user = response;
+            $scope.user.password = "xxxxxxxx";
 
             throttledUpdate("login");
             throttledUpdate("name");
             throttledUpdate("description");
             throttledUpdate("email");
+            throttledUpdate("email");
+            confirmedUpdate("password"); 
+            // Passwort will no be reset after update.
         });
     });
 }]);
