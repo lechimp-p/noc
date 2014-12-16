cddebf65fa7c025267ede6fe77929a25e6447b91
Richard Klees <richard.klees@rwth-aachen.de>
2014-12-16 19:48:26 +0100

    corrected notifications controller. no looks good.

--- HTML/custom/controllers/notifications.js
+++ HTML/custom/controllers/notifications.js
@@ -1,6 +1,6 @@
 angular.module("NoC.notifications", []).
 controller("notifications-controller", [ "$scope", "model", "user"
-         , function($rootScope, $scope, $routeParams, model, user) {
+         , function($scope, model, user) {
     "use strict";
 
     $scope.msgs = [];
@@ -23,83 +23,4 @@ controller("notifications-controller", [ "$scope", "model", "user"
         });
         model.user(id).notifications.update();
     });
-
-
-    $scope.post = function() {
-        if ($scope.message.length === 0) {
-            return;
-        }    
-
-        model.channel($scope.channel.id)
-            .messages
-            .post($scope.message);
-
-        $scope.message = "";
-    };
-
-    $scope.subscribe = function() {
-        user.subscribe($scope.channel.id);
-    };
-
-    $scope.unsubscribe = function() {
-        user.unsubscribe($scope.channel.id);
-    };
-
-    var setChannelInfo = function(data) {
-        $scope.channel = data;
-    };
-
-    var setMessages = function(msgs) {
-        $scope.msgs = msgs.messages;
-    };
-
-    var prependMessages = function(msgs) {
-        $scope.msgs = msgs.messages.concat($scope.msgs);
-    };
-
-    model.channel($scope.channel.id).onUpdate(setChannelInfo);
-
-    $rootScope.$on("$routeChangeStart", function(event, next, current) {
-        if (next.pathParams.chanId == $scope.channel.id) {
-            model.channel($scope.channel.id).update();
-            model.channel($scope.channel.id).messages.startUpdateTask(); 
-        }
-        else {
-            model.channel($scope.channel.id).messages.stopUpdateTask();
-        }
-    });
-
-    model.channel($scope.channel.id).update();
-    model.channel($scope.channel.id)
-        .messages.startUpdateTask();
-    model.channel( $scope.channel.id )
-        .messages.onUpdate( prependMessages );
-    model.channel( $scope.channel.id )
-        .messages.update();
-
-    //$interval($scope.updateMessages, updateIntervalMS);
-
-/*    $scope.msgs =
-        [ { author : 
-            { icon : "img/profilepic.png"
-            , name : "Max Mustermann"
-            }
-            , timestamp : "Do. 13:37"
-            , text : "Man, das ist ja supercrisp hier...."
-          }
-        , { author :
-            { icon : "img/profpic.jpg"
-            , name : "Petra Test"
-            }
-            , timestamp : "Fr. 23:55"
-            , text : "Ja, echt spitze!"
-          }
-        ];
-
-    $scope.channel =
-        { image : "img/channel/0.png"
-        , name : "Grillen in Kölle"
-        , description : "Für alle, die in Köln grillen wollen."
-        };
-*/
 }]);
